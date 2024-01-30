71df362a39d0d375a8ea85d86bb27aa210c393cb
amangithub9 <iamaman770@gmail.com>
2024-01-30 15:21:35 +0530

    Modification Done

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// const display = document.querySelector(".display");
-// const buttons = document.querySelectorAll("button");
-// const specialChar = ["%", "*", "/", "-", "+", "="];
-// let output = "";
-
-// const calculate = (btnValue) => {
-//   if (btnValue === "=" && output !== "") {
-//     output = eval(output.replace("%", "/100"));
-//   } else if (btnValue === "AC") {
-//     output = "";
-//   } else if (btnValue === "DEL") {
-//     output = output.toString().slice(0, -1);
-//   } else {
-//     if (output === "" && specialChar.includes(btnValue)) return;
-//     output = output + btnValue;
-//   }
-//   display.value = output;
-//   // console.log(btnValue);
-// };
-
-// buttons.forEach((button) => {
-//   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
-// });
-
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChar = ["%", "*", "/", "-", "+"];
